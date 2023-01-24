2c38a575586cd694f299ac590ee8a551070f1360
Joran Vanpeene <joran.vanpeene@gmail.com>
2023-01-24 10:39:45 +0100

    Tiers 24/01/23 10:39

--- client/src/Composants/Tiers/ListeTiers/ListeTiers.tsx
+++ client/src/Composants/Tiers/ListeTiers/ListeTiers.tsx
@@ -5,51 +5,19 @@ import "./ListeTiers.css"
 
 type Tiers = {
     _id: string,
-    nomTiers: string,
-    nomAlternatifTiers: string,
-    prospectClientTiers: string,
-    codeClientTiers: string,
-    fournisseurTiers: string,
-    codeFournisseurTiers: string,
-    etatTiers: string,
-    codeBarresTiers: string,
-    adresseTiers: string,
-    codePostalTiers: string,
-    villeTiers: string,
-    paysTiers: string,
-    departmentCantonTiers: string,
-    telephoneTiers: string,
-    faxTiers: string,
-    emailTiers: string,
-    webTiers: string,
-    skypeTiers: string,
-    twitterTiers: string,
-    linkedinTiers: string,
-    githubTiers: string,
-    idProf1Tiers: string,
-    idProf2Tiers: string,
-    idProf3Tiers: string,
-    idProf4Tiers: string,
-    idProf5Tiers: string,
-    assujettiTVATiers: string,
-    numeroTVATiers: string,
-    assujettiDeuxiemeTaxeTiers: string,
-    assujettiTroisiemeTaxeTiers: string,
-    typeTiers: string,
-    effectifsTiers: string,
-    typeEntiteLegaleTiers: string,
-    capitalTiers: string,
-    langueDefautTiers: string,
-    incotermsTiers: string,
-    tagsClientsProspTiers: string,
-    tagsFournisseursTiers: string,
-    tierseTiers: string,
-    hauteurTiers: string,
-    poidsTiers: string,
-    professionTiers: string,
-    dateNaissanceTiers: string,
-    maisonMereTiers: string,
-    commercialAffecteTiers: string,
+    code: string,
+    nom: string,
+    prenom: string,
+    adresse: string,
+    adresse2: string,
+    codepostal: string,
+    ville: string,
+    pays: string,
+    departement: string,
+    telephone:string,
+    email:string,
+    type: string,
+    commercial: string
 }
 
 function ListeTiers() {
@@ -67,36 +35,40 @@ function ListeTiers() {
             <MenuTiers></MenuTiers>
             <div className="listetiers-panel">
                 <h1>Liste tiers</h1>
+                <div className="listetierssearch">
+                    <h4>Recherche de Tiers</h4>
+                    <input type="text" placeholder="Nom du tiers"/>
+                </div>
                 <table className="listetiers-tab">
                     <thead className="listetiers-tab-entete">
                         <tr>
-                            <th>Nom du tiers</th>
-                            <th>Nom alternatif</th>
-                            <th>Code client</th>
+                            <th>Nom</th>
+                            <th>Prenom</th>
+                            <th>Adresse</th>
+                            <th>Seconde adresse</th>
+                            <th>Code Postal</th>
                             <th>Ville</th>
                             <th>Pays</th>
-                            <th>Type du tiers</th>
+                            <th>Departement</th>
+                            <th>Telephone</th>
                             <th>Email</th>
-                            <th>Téléphone</th>
-                            <th>Nature du tiers</th>
-                            <th>Maison mère</th>
-                            <th>Etat</th>
+                            <th>Type</th>
                         </tr>
                     </thead>
                     <tbody className="listetiers-tab-body">
                         {tiersList.map(tiers => 
                             <tr key={tiers._id}>
-                                <td>{tiers.nomTiers}</td>
-                                <td>{tiers.nomAlternatifTiers}</td>
-                                <td>{tiers.codeClientTiers}</td>
-                                <td>{tiers.villeTiers}</td>
-                                <td>{tiers.paysTiers}</td>
-                                <td>{tiers.typeTiers}</td>
-                                <td>{tiers.emailTiers}</td>
-                                <td>{tiers.telephoneTiers}</td>
-                                <td>{tiers.prospectClientTiers} et {tiers.fournisseurTiers}</td>
-                                <td>{tiers.maisonMereTiers}</td>
-                                <td>{tiers.etatTiers}</td>
+                                <td>{tiers.nom}</td>
+                                <td>{tiers.prenom}</td>
+                                <td>{tiers.adresse}</td>
+                                <td>{tiers.adresse2}</td>
+                                <td>{tiers.codepostal}</td>
+                                <td>{tiers.ville}</td>
+                                <td>{tiers.pays}</td>
+                                <td>{tiers.departement}</td>
+                                <td>{tiers.telephone}</td>
+                                <td>{tiers.email}</td>
+                                <td>{tiers.type}</td>
                             </tr>
                         )}
                     </tbody>
